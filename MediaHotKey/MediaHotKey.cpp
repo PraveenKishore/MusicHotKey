da73d586eb460e603b56de600173a973056be7c6
PraveenKishore <praveenkishorepraveen@gmail.com>
2016-08-05 23:39:36 +0530

    Updated MediaHotKey.cpp

--- MediaHotKey/MediaHotKey.cpp
+++ MediaHotKey/MediaHotKey.cpp
@@ -1,3 +1,11 @@
+/** Windows Music Shortcut Software to control your favorite Music Player by simulating Multimedia Key Events.
+* Don't have multimedia keys on your keyboard? Use MusicHotKey to Play/Pause, Play next track or Previous track using custom shortcut keys.
+*
+* Operating System: Windows
+* Programming Language: C++
+* Development Platform: Visual Studio 2010
+*/
+
 #include<iostream>
 #include<Windows.h>
 
